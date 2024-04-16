adc4844ba36f84b55f7279b10374890d7f2e6fe7
vikasgaddam9010 <116541237+vikasgaddam9010@users.noreply.github.com>
2024-04-16 23:33:56 +0530

    Planets App

--- src/components/PlanetsSlider/index.js
+++ src/components/PlanetsSlider/index.js
@@ -2,9 +2,6 @@ import PlanetItem from '../PlanetItem'
 
 import Slider from 'react-slick'
 
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-
 import './index.css'
 
 const PlanetsSlider = porps => {
