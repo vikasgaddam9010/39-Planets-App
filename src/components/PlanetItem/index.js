70aad05fc3b82c652d727dcfa254799ae1834cf2
vikasgaddam9010 <116541237+vikasgaddam9010@users.noreply.github.com>
2024-04-16 23:48:07 +0530

    Planets App

--- src/components/PlanetItem/index.js
+++ src/components/PlanetItem/index.js
@@ -4,7 +4,7 @@ const PlanetItem = props => {
   const {name, imageUrl, description} = props.eachPlanet
   return (
     <div className="container">
-      <img alt={`planet ${name}`} src={imageUrl} />
+      <img alt={`planet ${name}`} src={imageUrl}/>
       <h1 className="color-f8fafc">{name}</h1>
       <p className="color-f1f5f9">{description}</p>
     </div>
