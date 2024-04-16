7c5c980ac49070757658956b91776e5398226d4c
vikasgaddam9010 <vikasgaddam9010@gmail.com>
2024-04-16 18:01:05 +0000

    Planets App

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const PlanetItem = props => {
+  const {name, imageUrl, description} = props.eachPlanet
+  return (
+    <div className="container">
+      <img alt={`planet {name}`} src={imageUrl} />
+      <h1 className="color-f8fafc">{name}</h1>
+      <p className="color-f1f5f9">{description}</p>
+    </div>
+  )
+}
+export default PlanetItem
---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,32 @@
+import PlanetItem from '../PlanetItem'
+
+import Slider from 'react-slick'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import './index.css'
+
+const PlanetsSlider = porps => {
+  const {planetsList} = porps
+
+  const settings = {
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+  return (
+    <div  className="slider-container">
+      <h1 className="h1-planet">PLANETS</h1>
+      <Slider testid="planets" {...settings}>
+        {planetsList.map(eachPlanet => (
+          <div className="d-flex">
+            <PlanetItem key={eachPlanet.id} eachPlanet={eachPlanet} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
